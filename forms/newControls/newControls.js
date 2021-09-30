2dbe4dc03b198a340f01394a0443a1af6771d434
JackRyanCaton <jrc88858@creighton.edu>
2021-09-29 22:29:33 -0500

    newControls 1

---
+++ forms/newControls/newControls.js
@@ -0,0 +1,13 @@
+sldr1.onchange=function(){
+  let sliderValue = sldr1.getValue()
+  console.log(`You moved the slider to ${sliderValue}`)
+}
+
+cbx1.onclick=function(){
+  console.log("you made a selection")
+}
+
+jumboMessage.onclick=function(){
+  console.log("This is a message!")
+}
+
